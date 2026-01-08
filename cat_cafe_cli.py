2640c6b6c835226045267605c37bdf88e5f84104
keeganveazey <keeganveazey@gmail.com>
2026-01-08 15:23:30 -0500

    Init py

---
+++ cat_cafe_cli.py
@@ -0,0 +1,9 @@
+# my random fun side project since I love cats and don't want my coding skills to get too rusty
+
+def main():
+    print("Welcome to Keegan's Cat Cafe!")
+
+if __name__ == "__main__":
+    main()
+
+
